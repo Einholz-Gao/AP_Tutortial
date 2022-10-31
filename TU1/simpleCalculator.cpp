9a66d76ca6647a67bfd5350b5899c283952ab65a
Yichao Gao <yichao.gao@tum.de>
2022-10-31 11:26:33 +0100

    main

--- TU1/simpleCalculator.cpp
+++ TU1/simpleCalculator.cpp
@@ -1,20 +1,6 @@
 #include<iostream>
 using namespace std;
 //Alice creats it.
-double sum(double a,double b){
- return a+b;
-}
-double sum(double a,double b){
- return a-b;
-}
-
-double sum(double a, double b){
-    return a+b;
-}
-
-double subtract(double a, double b){
-    return a-b;
-}
 
 int main(){
     //write your code here.
