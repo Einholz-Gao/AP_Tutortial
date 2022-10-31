e4e39810dff83f5d0215b0ca4441d80d0425f608
Yichao Gao <yichao.gao@tum.de>
2022-10-31 11:17:04 +0100

    merge

--- TU1/simpleCalculator.cpp
+++ TU1/simpleCalculator.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 //Alice creats it.
 
+double sum(double a, double b){
+    return a+b;
+}
+
+double subtract(double a, double b){
+    return a-b;
+}
+
 int main(){
     //write your code here.
 
