080a3f15c3eef989830aa5fd17d8c698e5c075a7
Yichao Gao <yichao.gao@tum.de>
2022-11-01 09:58:59 +0100

    Update

--- TU1/simpleCalculator.cpp
+++ TU1/simpleCalculator.cpp
@@ -2,14 +2,39 @@
 using namespace std;
 //Alice creats it.
 
+double multiply(double a, double b){
+    return a*b;
+}
 
-double mean(double a, double b){
+double divide(double a,double b){
+    return b==0? 0: a/b;
+}
+
+
+double sum(double a,double b){
+ return a+b;
+}
+double subtract(double a,double b){
+ return a-b;
+}
+double mean(double a,double b)
+{
     return (a+b)/2;
 }
-
 int main(){
     //write your code here.
+    //cout<<divide(1,2)<<endl;
+    char c;
+    double a,b;
+    double res;
+    cin>>a>>c>>b;
+    if(c=='+')  res=sum(a,b);
+    else if(c=='-') res=subtract(a,b);        
+    else if(c=='*')  res=multiply(a,b);
+    else if(c=='/')   res=divide(a,b);
+    else res=mean(a,b);
 
-
+    cout<<res<<endl;
     return 0; 
 }
+
