813ee2d7dcd1f102f3707e65b2218aca610fa857
Yichao Gao <yichao.gao@tum.de>
2022-11-01 09:47:32 +0100

    fix

--- TU1/simpleCalculator.cpp
+++ TU1/simpleCalculator.cpp
@@ -1,14 +1,6 @@
 #include<iostream>
 using namespace std;
-//Bob creats it.
-
-double multiply(double a, double b){
-    return a*b;
-}
-
-double divide(double a,double b){
-    return b==0? 0: a/b;
-}
+//Alice creats it.
 
 
 double mean(double a, double b){
@@ -17,17 +9,7 @@ double mean(double a, double b){
 
 int main(){
     //write your code here.
-    //cout<<divide(1,2)<<endl;
-    char c;
-    double a,b;
-    double res;
-    cin>>a>>c>>b;
-    if(c=='+')  res=sum(a,b);
-    else if(c=='-') res=subtract(a,b);        
-    else if(c=='*')  res=multiply(a,b);
-    else if(c=='/')   res=divide(a,b);
-    else res=mean(a,b);
 
-    cout<<res<<endl;
+
     return 0; 
 }
