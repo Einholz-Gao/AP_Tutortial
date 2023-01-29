67fe78f53efdabafe6d641ed7967dee9991024b3
Yichao Gao <yichao.gao@tum.de>
2023-01-29 11:41:25 +0100

    update

---
+++ Tu9_STL/game-of-life/build/CMakeFiles/game-of-life.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/einholz/[Studitum]/WS2223/AP/Tutorial/Tu9_STL/game-of-life/game-of-life.cpp" "CMakeFiles/game-of-life.dir/game-of-life.cpp.o" "gcc" "CMakeFiles/game-of-life.dir/game-of-life.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Tu9_STL/game-of-life/build/CMakeFiles/game-of-life.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game-of-life.dir/game-of-life.cpp.o"
+  "CMakeFiles/game-of-life.dir/game-of-life.cpp.o.d"
+  "game-of-life"
+  "game-of-life.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game-of-life.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
